40bc66a3bac6561f90a5c314fd5fc8cf0a723dbc
manoj2123 <manojkumarga2103@gmail.com>
2023-08-04 17:46:21 +0530

    finished

---
+++ src/Data/data.js
@@ -0,0 +1,57 @@
+var data =  [
+    {
+    name : "Manoj kumar",
+    gender : "male",
+    batch: "B45WD",
+    qualification : "B.com IT"
+  },
+  {
+    name: "Rajeshwaran",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E"
+  },
+  {
+    name: "Anitha",
+    gender: "female",
+    batch: "B45WD",
+    qualification: "BSC IT"
+  },
+  {
+    name: "Akash",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "M.com"
+  },
+  {
+    name: "Anusha",
+    gender: "female",
+    batch: "B45WD",
+    qualification: "BCA"
+  },
+  {
+    name: "Kavin",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E-Mech"
+  },
+  {
+    name: "Mahesh",
+    gender: "male",
+    batch: "-",
+    qualification: "B.E-Mech"
+  },
+  {
+    name: "Kumar",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E-Mech"
+  },
+  {
+    name: "Suresh",
+    gender: "male",
+    batch: "B45WD",
+    qualification: "B.E-Mech"
+  }
+  ];
+  export default data;
